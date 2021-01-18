6ae7770f74062c348db33017e34b27257b81a3f3
Samuel Pike <sampike1994@gmail.com>
2021-01-18 13:56:48 -0500

    resolved-oauth

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 // figure out whether in dev or prod environment
 if (process.env.NODE_ENV === 'production') {
   // we are in production - return prod set of keys
-  module.exports = require('./dev');
+  module.exports = require('./prod');
 } else {
   //we in development - return the dev keys!!
   module.exports = require('./dev');
